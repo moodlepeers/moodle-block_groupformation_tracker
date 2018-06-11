de800c222fba54b84ad1fe16a51516a26d234470
Rene Roepke <r.roepke@online.de>
2018-06-11 19:07:10 +0200

    initial skeleton

---
+++ block_groupformation_tracker.php
@@ -0,0 +1,122 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Class block_pseudolearner
+ *
+ * @package block_groupformation_tracker
+ * @copyright 2018 MoodlePeers
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+class block_groupformation_tracker extends block_base {
+
+    /**
+     * Init function
+     */
+    public function init() {
+        $this->title = get_string('pluginname', 'block_groupformation_tracker');
+    }
+
+    /**
+     * On deletion of instance
+     *
+     * @return bool
+     * @throws dml_exception
+     */
+    public function instance_delete() {
+        global $DB;
+        $courseid = $this->page->course->id;
+
+        $DB->delete_records('block_groupformation_tracker', array('courseid' => $courseid));
+
+        return true;
+    }
+
+    /**
+     * On creation of instance
+     *
+     * @return bool
+     * @throws dml_exception
+     */
+    public function instance_create() {
+        global $DB;
+        $courseid = $this->page->course->id;
+        $record = new stdClass();
+        $record->courseid = $courseid;
+        $DB->insert_record('block_groupformation_tracker', $record);
+        return true;
+    }
+
+    /**
+     * Returns content object
+     *
+     * @return stdClass|stdObject|string
+     * @throws coding_exception
+     */
+    public function get_content() {
+
+        if ($this->content !== null) {
+            return $this->content;
+        }
+
+        if (empty($this->instance)) {
+            $this->content = '';
+            return $this->content;
+        }
+
+        $this->content = new stdClass();
+        $this->content->items = array();
+        $this->content->icons = array();
+        $this->content->footer = '';
+
+        // User/index.php expect course context, so get one if page has module context.
+        $currentcontext = $this->page->context->get_course_context(false);
+
+        $this->content = new stdClass();
+        $this->content->text = "Content here";
+        // use lang strings
+        //$this->content->text = get_string('string_identifier', 'block_groupformation_tracker');
+
+        if (empty($currentcontext)) {
+            return $this->content;
+        }
+
+        // Here you need to specify the content
+
+        return $this->content;
+    }
+
+    public function applicable_formats() {
+        return array('all' => false,
+                     'site' => true,
+                     'site-index' => true,
+                     'course-view' => true,
+                     'course-view-social' => false,
+                     'mod' => true,
+                     'mod-quiz' => false);
+    }
+
+    public function instance_allow_multiple() {
+          return false;
+    }
+
+    public function has_config() {
+        return true;
+    }
+}
---
+++ db/access.php
@@ -0,0 +1,63 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Defines additional capabilities
+ *
+ * @package block_groupformation_tracker
+ * @copyright 2018 MoodlePeers
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$capabilities = array(
+
+    'block/groupformation_tracker:myaddinstance' => array(
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => array(
+            'user' => CAP_ALLOW
+        ),
+
+        'clonepermissionsfrom' => 'moodle/my:manageblocks'
+    ),
+
+    'block/groupformation_tracker:editsettings' => array(
+        'riskbitmask'  => RISK_SPAM | RISK_PERSONAL | RISK_XSS | RISK_CONFIG,
+        'captype'      => 'write',
+        'contextlevel' => CONTEXT_MODULE,
+        'archetypes'   => array(
+            'student'        => CAP_PREVENT,
+            'teacher'        => CAP_ALLOW,
+            'editingteacher' => CAP_PREVENT,
+            'manager'          => CAP_ALLOW
+        )
+    ),
+
+    'block/groupformation_tracker:addinstance' => array(
+        'riskbitmask' => RISK_SPAM | RISK_XSS,
+
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+
+        'clonepermissionsfrom' => 'moodle/site:manageblocks'
+    ),
+);

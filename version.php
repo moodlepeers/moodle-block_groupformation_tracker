a304f442acb41796c20d2d97d2880781f62e0636
Rene Roepke <r.roepke@online.de>
2018-07-06 20:58:47 +0200

    version requirement set lower

--- version.php
+++ version.php
@@ -24,5 +24,5 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2018061100;                // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2018051700;                // Requires this Moodle version.
+$plugin->requires  = 2016052300;                // Requires this Moodle version.
 $plugin->component = 'block_groupformation_tracker';     // Full name of the plugin (used for diagnostics).

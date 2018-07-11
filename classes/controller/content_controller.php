41e7af3a5c22c225362e01622f344bd7970d3c24
sventi77 <sven.timpe@rwth-aachen.de>
2018-07-11 12:29:42 +0200

    added more teacher states

--- classes/controller/content_controller.php
+++ classes/controller/content_controller.php
@@ -144,7 +144,8 @@ public function get_teacher_content($groupformationid){
 
     $text = "";
     $activity_state = groupformation_get_activity_state($groupformationid);
-    //$text .= $activity_state;
+    $text .= $activity_state;
+    $text .= $groupformationid;
 
     switch ($activity_state){
         case "q_open":
@@ -156,27 +157,27 @@ public function get_teacher_content($groupformationid){
             break;
 
         case "gf_started":
-
+            $text .= $this->get_teacher_view_gf_started();
             break;
 
         case "gf_aborted":
-
+            $text .= $this->get_teacher_view_gf_aborted();
             break;
 
         case "gf_done":
-
+            $text .= $this->get_teacher_view_gf_done();
             break;
 
         case "ga_started":
-
+            $text .= $this->get_teacher_view_ga_started();
             break;
 
         case "ga_done":
-
+            $text .= $this->get_teacher_view_ga_done();
             break;
 
         case "q_reopened":
-
+            $text .= $this->get_teacher_view_reopened();
             break;
     }
 
@@ -206,11 +207,81 @@ public function get_teacher_view_open(){
 
 public function get_teacher_view_closed(){
 
-    $text = "<h3><span class=\"badge badge-pill badge-success\">open</span></h3>";
+    $text = "<h3><span class=\"badge badge-pill badge-danger\">closed</span></h3>";
     $text .= "<br>";
     $text .= "<a href=\"#\" class=\"btn btn-outline-primary\" role=\"button\" aria-pressed=\"true\">open questionnaire</a>";
     $text .= "<br><br>";
     $text .= "<a href=\"#\" class=\"btn btn-outline-primary\" role=\"button\" aria-pressed=\"true\">go to groupformation</a>";
+
+
+    return $text;
+}
+
+public function get_teacher_view_gf_started(){
+
+    $text = "<h3><span class=\"badge badge-pill badge-danger\">closed</span></h3>";
+    $text .= "<br>";
+    $text .= "<p>Please wait...<br>This process may take 2-5 min</p>";
+
+    return $text;
+}
+
+public function get_teacher_view_gf_aborted(){
+
+    $text = "<h3><span class=\"badge badge-pill badge-danger\">closed</span></h3>";
+    $text .= "<h3><span class=\"badge badge-pill badge-danger\">GF aborted</span></h3>";
+    $text .= "<br>";
+    $text .= "<a href=\"#\" class=\"btn btn-outline-primary\" role=\"button\" aria-pressed=\"true\">reset</a>";
+
+    return $text;
+}
+
+public function get_teacher_view_gf_done(){
+
+    $text = "<h3><span class=\"badge badge-pill badge-success\">GF generated</span></h3>";
+    $text .= "<br><br>";
+    $text .= "<a href=\"#\" class=\"btn btn-outline-primary\" role=\"button\" aria-pressed=\"true\">go to results</a>";
+
+    return $text;
+}
+
+public function get_teacher_view_ga_started(){
+
+    $text = "<h3><span class=\"badge badge-warning\">forming groups</span></h3>";
+    $text .= "<br>";
+    $text .= "<p>Please wait...</p>";
+
+    return $text;
+}
+
+public function get_teacher_view_ga_done(){
+
+    $text = "<h3><span class=\"badge badge-pill badge-danger\">groups adapted</span></h3>";
+    $text .= "<br>";
+    $text .= "<a href=\"#\" class=\"btn btn-outline-primary\" role=\"button\" aria-pressed=\"true\">delete groups</a>";
+    $text .= "<br><br>";
+    $text .= "<a href=\"#\" class=\"btn btn-outline-primary\" role=\"button\" aria-pressed=\"true\">reopen questionnaire</a>";
+
+
+    return $text;
+}
+
+public function get_teacher_view_reopened(){
+
+    $number_of_students = 100;
+    $students_ready = 60;
+    $progress = ($students_ready/$number_of_students)*100;
+    $text = "<h3><span class=\"badge badge-pill badge-success\">reopened</span></h3>";
+    $text .= "<br>";
+    $text .= "<div class=\"progress\"><div class=\"progress-bar progress-bar-striped progress-bar-animated\" role=\"progressbar\" aria-valuenow=\"";
+    $text .= $progress;
+    $text .= "\" aria-valuemin=\"0\" aria-valuemax=\"100\" style=\"width: ";
+    $text .= $progress;
+    $text .= "%\">";
+    $text .= $progress;
+    $text .= "%</div></div>";
+    $text .= "<br>";
+    $text .= "<a href=\"#\" class=\"btn btn-outline-primary\" role=\"button\" aria-pressed=\"true\">close questionnaire</a>";
 
 
     return $text;
